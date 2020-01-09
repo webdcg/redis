eff55d830ef4b73c07c93316239d5378ca6abaa1
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2020-01-09 03:26:27 +0000

    Apply fixes from StyleCI

--- src/Traits/Bits.php
+++ src/Traits/Bits.php
@@ -5,13 +5,13 @@ namespace Webdcg\Redis\Traits;
 trait Bits
 {
     /**
-     * Count set bits in a string
+     * Count set bits in a string.
      *
      * @param  string $key
      *
      * @return int
      */
-    public function bitCount(string $key) : int
+    public function bitCount(string $key): int
     {
         return $this->redis->bitCount($key);
     }
