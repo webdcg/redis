8632257fdca0bdc7d23e4e23bea7dbe9cdcb3b74
Roberto Luna Rojas <rluna@pch.com>
2020-01-15 22:06:59 -0500

    Redis | Scripting | Trait

---
+++ src/Traits/Scripting.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Webdcg\Redis\Traits;
+
+trait Scripting
+{
+    public function eval():bool
+    {
+        return false;
+    }
+    public function evalSha():bool
+    {
+        return false;
+    }
+    public function script():bool
+    {
+        return false;
+    }
+    public function getLastError():bool
+    {
+        return false;
+    }
+    public function clearLastError():bool
+    {
+        return false;
+    }
+    public function prefix():bool
+    {
+        return false;
+    }
+    public function unserialize():bool
+    {
+        return false;
+    }
+    public function serialize():bool
+    {
+        return false;
+    }
+}
