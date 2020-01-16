bcf7f8e5c0b32a19d834274b038f35ecdacc831d
Roberto Luna Rojas <rluna@pch.com>
2020-01-15 22:11:47 -0500

    Redis | Pubsub | Trait

---
+++ src/Traits/Pubsub.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Webdcg\Redis\Traits;
+
+trait Pubsub
+{
+    public function pSubscribe():bool
+    {
+        return false;
+    }
+    public function publish():bool
+    {
+        return false;
+    }
+    public function subscribe():bool
+    {
+        return false;
+    }
+    public function pubSub():bool
+    {
+        return false;
+    }
+}
