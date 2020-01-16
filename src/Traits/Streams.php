33674bcd85bd3c3f04ac588b3152be850fb250c3
Roberto Luna Rojas <rluna@pch.com>
2020-01-15 22:43:40 -0500

    Redis | Lists | Trait

--- src/Traits/Streams.php
+++ src/Traits/Streams.php
@@ -8,54 +8,64 @@ trait Streams
     {
         return false;
     }
+    
     public function xAdd():bool
     {
         return false;
     }
+    
     public function xClaim():bool
     {
         return false;
     }
+    
     public function xDel():bool
     {
         return false;
     }
+    
     public function xGroup():bool
     {
         return false;
     }
+    
     public function xInfo():bool
     {
         return false;
     }
+    
     public function xLen():bool
     {
         return false;
     }
+    
     public function xPending():bool
     {
         return false;
     }
+    
     public function xRange():bool
     {
         return false;
     }
+    
     public function xRead():bool
     {
         return false;
     }
+    
     public function xReadGroup():bool
     {
         return false;
     }
+    
     public function xRevRange():bool
     {
         return false;
     }
+
     public function xTrim():bool
     {
         return false;
     }
-    public function
-}():bool{return false;}
 }
