c266e8b62b7afabfee85c8a249dcd087f6f28742
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2020-01-15 22:10:00 -0500

    Apply fixes from StyleCI (#23)

--- src/Traits/Transactions.php
+++ src/Traits/Transactions.php
@@ -4,23 +4,27 @@ namespace Webdcg\Redis\Traits;
 
 trait Transactions
 {
-    public function multi():bool
+    public function multi(): bool
     {
         return false;
     }
-    public function exec():bool
+
+    public function exec(): bool
     {
         return false;
     }
-    public function discard():bool
+
+    public function discard(): bool
     {
         return false;
     }
-    public function watch():bool
+
+    public function watch(): bool
     {
         return false;
     }
-    public function unwatch():bool
+
+    public function unwatch(): bool
     {
         return false;
     }
