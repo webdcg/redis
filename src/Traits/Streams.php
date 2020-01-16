2851598cb1b014f2b325c0baa24200c410f0ada9
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2020-01-15 22:43:57 -0500

    Apply fixes from StyleCI (#29)

--- src/Traits/Streams.php
+++ src/Traits/Streams.php
@@ -4,67 +4,67 @@ namespace Webdcg\Redis\Traits;
 
 trait Streams
 {
-    public function xAck():bool
-    {
-        return false;
-    }
-    
-    public function xAdd():bool
-    {
-        return false;
-    }
-    
-    public function xClaim():bool
-    {
-        return false;
-    }
-    
-    public function xDel():bool
-    {
-        return false;
-    }
-    
-    public function xGroup():bool
-    {
-        return false;
-    }
-    
-    public function xInfo():bool
-    {
-        return false;
-    }
-    
-    public function xLen():bool
-    {
-        return false;
-    }
-    
-    public function xPending():bool
-    {
-        return false;
-    }
-    
-    public function xRange():bool
-    {
-        return false;
-    }
-    
-    public function xRead():bool
-    {
-        return false;
-    }
-    
-    public function xReadGroup():bool
-    {
-        return false;
-    }
-    
-    public function xRevRange():bool
+    public function xAck(): bool
     {
         return false;
     }
 
-    public function xTrim():bool
+    public function xAdd(): bool
+    {
+        return false;
+    }
+
+    public function xClaim(): bool
+    {
+        return false;
+    }
+
+    public function xDel(): bool
+    {
+        return false;
+    }
+
+    public function xGroup(): bool
+    {
+        return false;
+    }
+
+    public function xInfo(): bool
+    {
+        return false;
+    }
+
+    public function xLen(): bool
+    {
+        return false;
+    }
+
+    public function xPending(): bool
+    {
+        return false;
+    }
+
+    public function xRange(): bool
+    {
+        return false;
+    }
+
+    public function xRead(): bool
+    {
+        return false;
+    }
+
+    public function xReadGroup(): bool
+    {
+        return false;
+    }
+
+    public function xRevRange(): bool
+    {
+        return false;
+    }
+
+    public function xTrim(): bool
     {
         return false;
     }
