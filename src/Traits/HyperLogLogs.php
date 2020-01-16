96040ec70881790d39d8995d37f1fdb1f4cac0a2
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2020-01-16 03:28:16 +0000

    Apply fixes from StyleCI

--- src/Traits/HyperLogLogs.php
+++ src/Traits/HyperLogLogs.php
@@ -4,15 +4,17 @@ namespace Webdcg\Redis\Traits;
 
 trait HyperLogLogs
 {
-    public function pfAdd():bool
+    public function pfAdd(): bool
     {
         return false;
     }
-    public function pfCount():bool
+
+    public function pfCount(): bool
     {
         return false;
     }
-    public function pfMerge():bool
+
+    public function pfMerge(): bool
     {
         return false;
     }
