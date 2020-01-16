c0c11b18b86d07f92550e61530d73572b4bacb1a
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2020-01-16 03:11:55 +0000

    Apply fixes from StyleCI

--- src/Traits/Pubsub.php
+++ src/Traits/Pubsub.php
@@ -4,19 +4,22 @@ namespace Webdcg\Redis\Traits;
 
 trait Pubsub
 {
-    public function pSubscribe():bool
+    public function pSubscribe(): bool
     {
         return false;
     }
-    public function publish():bool
+
+    public function publish(): bool
     {
         return false;
     }
-    public function subscribe():bool
+
+    public function subscribe(): bool
     {
         return false;
     }
-    public function pubSub():bool
+
+    public function pubSub(): bool
     {
         return false;
     }
