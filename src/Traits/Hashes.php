51d894be29f5c029c5ad9f6cd48ac828127c52e3
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2020-01-15 22:47:29 -0500

    Apply fixes from StyleCI (#30)

--- src/Traits/Hashes.php
+++ src/Traits/Hashes.php
@@ -8,72 +8,72 @@ trait Hashes
     {
         return false;
     }
-    
+
     public function hExists(): bool
     {
         return false;
     }
-    
+
     public function hGet(): bool
     {
         return false;
     }
-    
+
     public function hGetAll(): bool
     {
         return false;
     }
-    
+
     public function hIncrBy(): bool
     {
         return false;
     }
-    
+
     public function hIncrByFloat(): bool
     {
         return false;
     }
-    
+
     public function hKeys(): bool
     {
         return false;
     }
-    
+
     public function hLen(): bool
     {
         return false;
     }
-    
+
     public function hMGet(): bool
     {
         return false;
     }
-    
+
     public function hMSet(): bool
     {
         return false;
     }
-    
+
     public function hSet(): bool
     {
         return false;
     }
-    
+
     public function hSetNx(): bool
     {
         return false;
     }
-    
+
     public function hVals(): bool
     {
         return false;
     }
-    
+
     public function hScan(): bool
     {
         return false;
     }
-    
+
     public function hStrLen(): bool
     {
         return false;
