8a5e3450bbcebc1bab1abc593243a2912aee2409
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2020-01-18 17:41:43 -0500

    Apply fixes from StyleCI (#47)

--- tests/RedisGeocodingTest.php
+++ tests/RedisGeocodingTest.php
@@ -21,8 +21,8 @@ class RedisGeocodingTest extends TestCase
         // Start from scratch
         $this->assertGreaterThanOrEqual(0, $this->redis->delete('Geocoding'));
         // Add San Francisco to the geospatial key should only do it once
-        $this->assertEquals(1, $this->redis->geoAdd('Geocoding', -122.431, 37.773, "San Francisco"));
-        $this->assertEquals(0, $this->redis->geoAdd('Geocoding', -122.431, 37.773, "San Francisco"));
+        $this->assertEquals(1, $this->redis->geoAdd('Geocoding', -122.431, 37.773, 'San Francisco'));
+        $this->assertEquals(0, $this->redis->geoAdd('Geocoding', -122.431, 37.773, 'San Francisco'));
         // Cleanup used keys
         $this->assertGreaterThanOrEqual(0, $this->redis->delete('Geocoding'));
     }
