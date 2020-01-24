5371960f555db94211c0144fb64d4915a84e4e2d
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2020-01-24 11:39:59 -0500

    More Redis Bits Test updates for Keys

--- tests/RedisBitsTest.php
+++ tests/RedisBitsTest.php
@@ -10,12 +10,14 @@ class RedisBitsTest extends TestCase
 {
     protected $redis;
     protected $key;
+    protected $keyOptional;
 
     protected function setUp(): void
     {
         $this->redis = new Redis;
         $this->redis->connect();
         $this->key = 'Bits';
+        $this->keyOptional = 'BitsOptional';
     }
 
     /**
@@ -77,63 +79,63 @@ class RedisBitsTest extends TestCase
     public function redis_bits_bitop_and_operation()
     {
         // Set a couple strings
-        $this->assertTrue($this->redis->set('testBit1', 0));
-        $this->assertTrue($this->redis->set('testBit2', 1));
-        $this->assertEquals(2, $this->redis->bitCount('testBit1'));
-        $this->assertEquals(3, $this->redis->bitCount('testBit2'));
+        $this->assertTrue($this->redis->set($this->key, 0));
+        $this->assertTrue($this->redis->set($this->keyOptional, 1));
+        $this->assertEquals(2, $this->redis->bitCount($this->key));
+        $this->assertEquals(3, $this->redis->bitCount($this->keyOptional));
 
         // Perform an AND bitwise operation between the two
-        $this->assertEquals(1, $this->redis->bitOp('and', 'testBitOpAnd', 'testBit1', 'testBit2'));
+        $this->assertEquals(1, $this->redis->bitOp('and', 'testBitOpAnd', $this->key, $this->keyOptional));
 
         // Check that the original values remain
-        $this->assertEquals(0, $this->redis->get('testBit1'));
-        $this->assertEquals(1, $this->redis->get('testBit2'));
+        $this->assertEquals(0, $this->redis->get($this->key));
+        $this->assertEquals(1, $this->redis->get($this->keyOptional));
 
         // Verify the opput of the operation
         $this->assertEquals(0, $this->redis->get('testBitOpAnd'));
         $this->assertEquals(2, $this->redis->bitCount('testBitOpAnd'));
 
         // Remove all the keys used
-        $this->assertEquals(3, $this->redis->delete(['testBit1', 'testBit2', 'testBitOpAnd']));
+        $this->assertEquals(3, $this->redis->delete([$this->key, $this->keyOptional, 'testBitOpAnd']));
     }
 
     /** @test */
     public function redis_bits_bitop_or_operation()
     {
-        $this->assertGreaterThanOrEqual(0, $this->redis->delete('testBit1'));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit1', 0, 0));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit1', 1, 1));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit1', 2, 0));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit1', 3, 1));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit1', 4, 0));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit1', 5, 1));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit1', 6, 0));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit1', 7, 1));
-        $this->assertEquals(4, $this->redis->bitCount('testBit1'));
-        $value = $this->redis->get('testBit1');
+        $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->key));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->key, 0, 0));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->key, 1, 1));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->key, 2, 0));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->key, 3, 1));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->key, 4, 0));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->key, 5, 1));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->key, 6, 0));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->key, 7, 1));
+        $this->assertEquals(4, $this->redis->bitCount($this->key));
+        $value = $this->redis->get($this->key);
         $this->assertEquals('1010101', $this->getBinaryString($value));
         $this->assertEquals(85, ord($value));
 
-        $this->assertGreaterThanOrEqual(0, $this->redis->delete('testBit2'));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit2', 0, 1));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit2', 1, 0));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit2', 2, 1));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit2', 3, 0));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit2', 4, 1));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit2', 5, 0));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit2', 6, 1));
-        $this->assertGreaterThanOrEqual(0, $this->redis->setBit('testBit2', 7, 0));
-        $this->assertEquals(4, $this->redis->bitCount('testBit2'));
-        $value = $this->redis->get('testBit2');
+        $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->keyOptional));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->keyOptional, 0, 1));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->keyOptional, 1, 0));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->keyOptional, 2, 1));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->keyOptional, 3, 0));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->keyOptional, 4, 1));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->keyOptional, 5, 0));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->keyOptional, 6, 1));
+        $this->assertGreaterThanOrEqual(0, $this->redis->setBit($this->keyOptional, 7, 0));
+        $this->assertEquals(4, $this->redis->bitCount($this->keyOptional));
+        $value = $this->redis->get($this->keyOptional);
         $this->assertEquals('10101010', $this->getBinaryString($value));
         $this->assertEquals(170, ord($value));
 
-        $this->assertEquals(1, $this->redis->bitOp('or', 'testBitOpOr', 'testBit1', 'testBit2'));
+        $this->assertEquals(1, $this->redis->bitOp('or', 'testBitOpOr', $this->key, $this->keyOptional));
         $value = $this->redis->get('testBitOpOr');
         $this->assertEquals('11111111', $this->getBinaryString($value));
 
         // Remove all the keys used
-        $this->assertEquals(3, $this->redis->delete(['testBit1', 'testBit2', 'testBitOpOr']));
+        $this->assertEquals(3, $this->redis->delete([$this->key, $this->keyOptional, 'testBitOpOr']));
     }
 
     /** @test */
