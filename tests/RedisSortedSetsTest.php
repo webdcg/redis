13689736283e8dcfa859b0d354fa939753112ab8
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2020-02-05 09:39:46 -0500

    Apply fixes from StyleCI (#98)

--- tests/RedisSortedSetsTest.php
+++ tests/RedisSortedSetsTest.php
@@ -29,7 +29,7 @@ class RedisSortedSetsTest extends TestCase
         // Start from scratch
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->key));
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->keyOptional));
-        
+
         $this->assertEquals(1, $this->redis->zAdd($this->key, 1.0, 'A'));
         $this->assertEquals(1, $this->redis->zAdd($this->key, 2.5, 'B'));
         $this->assertEquals(2, $this->redis->zCard($this->key));
@@ -41,7 +41,7 @@ class RedisSortedSetsTest extends TestCase
         // T E S T  -----------------------------------------------------------
         $this->assertEquals(1, $this->redis->zInter($destinationKey, [$this->key, $this->keyOptional], [1, 1], 'MAX'));
         $this->assertEquals(1, $this->redis->zCard($destinationKey));
-        
+
         // Remove all the keys used
         $this->assertEquals(1, $this->redis->delete($this->key));
         $this->assertEquals(1, $this->redis->delete($this->keyOptional));
@@ -54,7 +54,7 @@ class RedisSortedSetsTest extends TestCase
         // Start from scratch
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->key));
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->keyOptional));
-        
+
         $this->assertEquals(1, $this->redis->zAdd($this->key, 1.0, 'A'));
         $this->assertEquals(1, $this->redis->zAdd($this->key, 2.5, 'B'));
         $this->assertEquals(2, $this->redis->zCard($this->key));
@@ -66,7 +66,7 @@ class RedisSortedSetsTest extends TestCase
         // T E S T  -----------------------------------------------------------
         $this->assertEquals(1, $this->redis->zInter($destinationKey, [$this->key, $this->keyOptional], [1, 1], 'MIN'));
         $this->assertEquals(1, $this->redis->zCard($destinationKey));
-        
+
         // Remove all the keys used
         $this->assertEquals(1, $this->redis->delete($this->key));
         $this->assertEquals(1, $this->redis->delete($this->keyOptional));
@@ -80,7 +80,7 @@ class RedisSortedSetsTest extends TestCase
         // Start from scratch
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->key));
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->keyOptional));
-        
+
         $this->assertEquals(1, $this->redis->zAdd($this->key, 1.0, 'A'));
         $this->assertEquals(1, $this->redis->zAdd($this->key, 2.0, 'B'));
         $this->assertEquals(2, $this->redis->zCard($this->key));
@@ -92,7 +92,7 @@ class RedisSortedSetsTest extends TestCase
         // T E S T  -----------------------------------------------------------
         $this->assertEquals(1, $this->redis->zInter($destinationKey, [$this->key, $this->keyOptional], [1, 1], 'SUM'));
         $this->assertEquals(1, $this->redis->zCard($destinationKey));
-        
+
         // Remove all the keys used
         $this->assertEquals(1, $this->redis->delete($this->key));
         $this->assertEquals(1, $this->redis->delete($this->keyOptional));
@@ -106,7 +106,7 @@ class RedisSortedSetsTest extends TestCase
         // Start from scratch
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->key));
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->keyOptional));
-        
+
         $this->assertEquals(1, $this->redis->zAdd($this->key, 1.0, 'A'));
         $this->assertEquals(1, $this->redis->zAdd($this->key, 2.0, 'B'));
         $this->assertEquals(2, $this->redis->zCard($this->key));
@@ -118,7 +118,7 @@ class RedisSortedSetsTest extends TestCase
         // T E S T  -----------------------------------------------------------
         $this->assertEquals(1, $this->redis->zInter($destinationKey, [$this->key, $this->keyOptional], [2, 4]));
         $this->assertEquals(1, $this->redis->zCard($destinationKey));
-        
+
         // Remove all the keys used
         $this->assertEquals(1, $this->redis->delete($this->key));
         $this->assertEquals(1, $this->redis->delete($this->keyOptional));
@@ -132,7 +132,7 @@ class RedisSortedSetsTest extends TestCase
         // Start from scratch
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->key));
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->keyOptional));
-        
+
         $this->assertEquals(1, $this->redis->zAdd($this->key, 1.0, 'A'));
         $this->assertEquals(1, $this->redis->zAdd($this->key, 2.0, 'B'));
         $this->assertEquals(2, $this->redis->zCard($this->key));
@@ -144,7 +144,7 @@ class RedisSortedSetsTest extends TestCase
         // T E S T  -----------------------------------------------------------
         $this->assertEquals(1, $this->redis->zInter($destinationKey, [$this->key, $this->keyOptional], [1, 1]));
         $this->assertEquals(1, $this->redis->zCard($destinationKey));
-        
+
         // Remove all the keys used
         $this->assertEquals(1, $this->redis->delete($this->key));
         $this->assertEquals(1, $this->redis->delete($this->keyOptional));
@@ -158,7 +158,7 @@ class RedisSortedSetsTest extends TestCase
         // Start from scratch
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->key));
         $this->assertGreaterThanOrEqual(0, $this->redis->delete($this->keyOptional));
-        
+
         $this->assertEquals(1, $this->redis->zAdd($this->key, 1.0, 'A'));
         $this->assertEquals(1, $this->redis->zAdd($this->key, 2.0, 'B'));
         $this->assertEquals(2, $this->redis->zCard($this->key));
